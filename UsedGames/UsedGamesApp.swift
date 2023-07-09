572d3259b057b8716c12c5d0ff75166126b957c6
cesurkedi <cesurkedixx@protonmail.com>
2023-07-09 20:31:36 +0300

    Initial Commit

---
+++ UsedGames/UsedGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesApp.swift
+//  UsedGames
+//
+//  Created by Çare C. on 9.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
